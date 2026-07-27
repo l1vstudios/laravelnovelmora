a4f2cf957a84697e57389c9a61376bfdce239e65
L1V <donikasinos66@gmail.com>
2026-07-27 12:48:29 +0700

    Push

--- resources/views/content/auth/login.blade.php
+++ resources/views/content/auth/login.blade.php
@@ -21,13 +21,13 @@
           </div>
           {{-- /Logo --}}
 
-          <h4 class="mb-1 pt-2">
+          {{-- <h4 class="mb-1 pt-2">
             Selamat Datang!
           </h4>
 
           <p class="mb-6 text-muted">
             Masuk ke akun admin Anda untuk melanjutkan
-          </p>
+          </p> --}}
 
           {{-- Global Error --}}
           @if ($errors->any())
