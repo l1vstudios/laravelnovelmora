e41ef7e0a9d50b5be98d82c184e3ef0fe9be26c2
L1V <donikasinos66@gmail.com>
2026-09-11 15:20:16 +0700

    Push

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Console\ClosureCommand;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
@@ -10,4 +11,19 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('reward:reset-daily')->dailyAt('00:00');
+
+Schedule::command('reward:reset-daily')
+    ->dailyAt('00:00')
+    ->timezone('Asia/Jakarta')
+    ->withoutOverlapping();
+
+
+Schedule::call(function () {
+    DB::statement(
+        'REFRESH MATERIALIZED VIEW CONCURRENTLY mv_cerita_preview'
+    );
+})
+    ->name('refresh-mv-cerita-preview')
+    ->dailyAt('00:00')
+    ->timezone('Asia/Jakarta')
+    ->withoutOverlapping();
